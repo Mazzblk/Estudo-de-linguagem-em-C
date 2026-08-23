f1cb67eea14aee515cc7725f89d6723c6f3e1d6d
SilentF <fernandohhm2004@gmail.com>
2026-08-23 01:51:34 -0300

    Desaafio 04 em C. O seafio esa ultilizar break e goto.

---
+++ Desafio/Desafio 04/Desafio04.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+    float md[5], nt[3], sm;
+    int i, j, qn;
+    char nm[5][50];
+    qn = 0;
+    do{ 
+        sm = 0;
+        printf("Registrar Notas\n\n");
+        for (i = 0; i != 3; i++){
+            printf("Escreva a nota %d: ", i+1);
+            scanf("%f", &nt[i]); 
+            if (nt[i] == -1){
+                goto fim_programa;
+            } 
+            if ((nt[i] > 100) || (nt[i] < 0)){
+            break;
+            }  
+        }
+        if ((nt[i] > 100) || (nt[i] < 0)){
+            break;
+        }
+        for (i = 0; i != 3; i++){
+            sm += nt[i];  
+        }
+        printf("Escreva o nome do aluno %d: ", qn+1);
+        scanf(" %49[^\n]", nm[qn]);
+        system("cls");
+        md[qn] = sm/3; 
+        if (qn == 4){
+            goto fim_programa;
+        }
+        qn++;
+    }while (1);
+    system("cls");
+    printf("Erro: Valor de nota invalido.");
+    goto f;
+    fim_programa:
+    system("cls");
+    printf("\nNotas registradas:\n");
+    for (i = 0; i != qn; i++){
+        printf("%d: %s - %.1f\n", i+1, nm[i], md[i]);
+    }
+    f:
+    return 0;
+}
